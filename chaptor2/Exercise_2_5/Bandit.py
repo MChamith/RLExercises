9e0fcc015ae298adf4e4a089cc1a40b4fe90898d
chamith.m <chamith.m@meetsid.com>
2023-07-21 14:49:25 +0300

    Initial commit

---
+++ chaptor2/Exercise_2_5/Bandit.py
@@ -0,0 +1,79 @@
+import random
+
+import numpy as np
+
+
+class Bandit():
+    def __init__(self, num_of_actions):
+        self.k = num_of_actions
+        self.act_val_distrib = [np.random.normal(0, 1, 10) for i in range(self.k)]
+        self.action_values = [np.mean(self.act_val_distrib[i]) for i in range(self.k)]
+        self.Q = np.full(self.k, 0, dtype=float)
+        self.N = np.full(self.k, 0)
+
+    def get_reward(self, i):
+        return self.action_values[i]
+
+    def explore(self):
+        raise NotImplementedError("Not Implemented")
+
+    def exploit(self):
+        raise NotImplementedError("Not implemented")
+
+    def is_optimal(self, action):
+        return float(action == np.argmax(self.action_values))
+
+    def random_walk(self):
+        increment = [np.random.normal(0, 0.01, 100) for i in range(self.k)]
+        self.act_val_distrib += increment
+        self.action_values = [np.mean(self.act_val_distrib[i]) for i in range(self.k)]
+
+
+class SampleAverageBandit(Bandit):
+
+    def __init__(self, num_of_actions):
+        super(SampleAverageBandit, self).__init__(num_of_actions)
+
+    def explore(self):
+        a = random.randint(0, self.k - 1)
+        # print('explore selected action ' + str(a + 1))
+        reward = self.get_reward(a)
+        self.N[a] += 1
+        self.Q[a] = self.Q[a] + (1 / self.N[a]) * (reward - self.Q[a])
+        return a, reward
+
+    def exploit(self):
+        a = np.argmax(self.Q)
+        # print('exploit action ' + str(a))
+        reward = self.get_reward(a)
+        self.N[a] += 1
+        self.Q[a] = self.Q[a] + (1 / self.N[a]) * (reward - self.Q[a])
+        return a, reward
+
+class WeightedAverageBandit(Bandit):
+
+    def __init__(self, num_of_actions, step_size):
+        super(WeightedAverageBandit, self).__init__(num_of_actions)
+        self.step_size = step_size
+
+    def explore(self):
+        a = random.randint(0, self.k - 1)
+        # print('explore selected action ' + str(a + 1))
+        reward = self.get_reward(a)
+        self.N[a] += 1
+        self.Q[a] = self.Q[a] + (1 / self.step_size) * (reward - self.Q[a])
+        return a, reward
+
+    def exploit(self):
+        a = np.argmax(self.Q)
+        # print('exploit action ' + str(a))
+        reward = self.get_reward(a)
+        self.N[a] += 1
+        self.Q[a] = self.Q[a] + (1 / self.step_size) * (reward - self.Q[a])
+        return a, reward
+
+def get_instance(bandit_mode, k, alpha = 0.1):
+    if bandit_mode == 'sample':
+        return SampleAverageBandit(k)
+    elif bandit_mode == 'weighted':
+        return WeightedAverageBandit(k, step_size= alpha)
---
+++ chaptor2/Exercise_2_5/bandit_task.py
@@ -0,0 +1,42 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from Bandit import get_instance
+
+
+def perform_task(k, epsilon, N_STEPS, N_RUNS, mode, alpha=0.1):
+    Optimal_percentage = np.full((N_RUNS, N_STEPS), 0, dtype=float)
+    Reward = np.full((N_RUNS, N_STEPS), 0, dtype=float)
+
+    for run_i in tqdm(range(1, N_RUNS + 1)):
+        bandit = get_instance(mode, k, alpha)
+        for step in range(1, N_STEPS + 1):
+
+            prob = random.uniform(0, 1)
+
+            if prob < epsilon:
+                step_action, reward = bandit.explore()
+            else:
+                step_action, reward = bandit.exploit()
+
+            optimal = bandit.is_optimal(step_action)
+
+            Optimal_percentage[run_i - 1][step - 1] = optimal
+            Reward[run_i - 1][step - 1] = reward
+            bandit.random_walk()
+
+    return Optimal_percentage, Reward
+
+
+percent_optimal_sample, reward_sample = perform_task(10, 0.1, 1000, 200, 'sample')
+avg_optimal_sample = np.mean(percent_optimal_sample, axis=0)
+avg_reward_sample = np.mean(reward_sample, axis=0)
+
+# percent_optimal_weighted, reward_weighted = perform_task(10, 0.1, 10000, 2000, 'weighted')
+# avg_optimal_weighted = np.mean(percent_optimal_weighted, axis=0)
+# avg_reward_weighted = np.mean(reward_weighted, axis=0)
+
+plt.plot(avg_optimal_sample)
+plt.plot(avg_reward_sample)
+plt.show()
